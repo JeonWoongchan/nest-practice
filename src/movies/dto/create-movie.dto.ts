6f4ef36d0a4378a31fd887ad8cc24ace17650042
woongchan <jchan715@globalbridge.co.kr>
2024-05-07 17:35:32 +0900

    0507-1

---
+++ src/movies/dto/create-movie.dto.ts
@@ -0,0 +1,23 @@
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateMovieDto {
+  @ApiProperty({
+    example: 'superman', // 예시 데이터
+    description: '영화 제목', // 설명
+    required: true, // 필수 전송 데이터 설정 (true / false)
+  })
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @ApiProperty({
+    example: '2024',
+    description: '개봉 일자',
+    type: 'number', // 데이터 타입 (기본값은 string)
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  year: number;
+}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
-@Controller()
+@ApiTags('사용자 API')
+@Controller('/movies')
 export class MoviesController {
   @Get()
   getAll() {
@@ -12,9 +23,11 @@ export class MoviesController {
     return `One Movie id = ${movieId}`;
   }
 
+  @ApiOperation({ summary: '영화 등록 API', description: '영화 추가' })
   @Post()
-  create() {
-    return `create Movie`;
+  create(@Body() createMovieDto: CreateMovieDto) {
+    console.log(createMovieDto);
+    return 'create complete';
   }
 
   @Delete('/:id')
